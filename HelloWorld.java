40031a7cf919482514420eddd31cb078876e4fc0
Mike Boan <mikeboan@gmail.com>
2017-10-03 10:07:07 -0400

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+import edu.princeton.cs.algs4.*;
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        WeightedQuickUnionUF wquuf = new WeightedQuickUnionUF(5); 
+        System.out.println("hello, world");
+    }
+}
